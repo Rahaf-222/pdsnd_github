861a01d5ecda991c3db9d258c1c8a34083016c81
Rahaf Alshehri <Rahaf.dhafer44@gmail.com>
2022-12-21 09:11:24 +0300

    refactor: edit the message

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -40,7 +40,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hi! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = check_input("Would you like to see the data for chicago, new york city or washington?",1)
     # TO DO: get user input for month (all, january, february, ... , june)
@@ -209,6 +209,7 @@ def main():
             view_raw_data = input('\nWould you like to view first five row of raw data? Enter yes or no.\n')
             if view_raw_data.lower() != 'yes':
                 break
+                 """Displays raw data on user request.""
             display_raw_data(df)
             break
 
